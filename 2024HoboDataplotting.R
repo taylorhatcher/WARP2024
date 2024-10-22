c6b0947c2795ca37b5c10a36a3649aa3bf69423c
Taylor Hatcher <129802014+taylorhatcher@users.noreply.github.com>
2024-10-22 13:54:57 -0700

    Update 2024HoboDataplotting.R

--- 2024HoboDataplotting.R
+++ 2024HoboDataplotting.R
@@ -1,45 +1,63 @@
 # Plotting operative model temperatures from 2024 field season
 # Taylor M. Hatcher
 #
+# Plotting operative model temperatures from 2024 field season
+# Taylor M. Hatcher
+
 library(ggplot2)
 library(dplyr)
 library(lubridate)
 library(cowplot)
 library(tidyr)
 
-# Read data from Hobo1 CSV file
-hobo1data2024 <- read.csv('Hobo_1_2024field.csv',
-                          sep = ',',
-                          header = FALSE,
-                          skip = 3,
-                          colClasses= c("character", "character", "numeric", "numeric", "character"),
-                          na.strings = c("Logged", "Series:T-Type")) %>%
-  mutate(dt = mdy_hms(V1)) 
+# Load in logger data for each logger and process datetime
+# Use 'character' for temp columns initially to handle possible non-numeric entries
+hobo1data2024 <- read.csv('Hobo_1_2024field.csv', sep = ',', header = FALSE, skip = 3,
+                          colClasses = c("character", "character", "character", "character", "character"),
+                          na.strings = c("Logged", "Series: T-Type")) %>%
+  mutate(datetime = mdy_hms(V1)) %>%
+  mutate(across(starts_with("V"), as.numeric))  # Convert temp columns to numeric
 
-# Read data from Hobo2 CSV file 
-hobo2data2024 <- read.csv('Hobo_2_2024field.csv',
-                          sep = ',',
-                          header = FALSE,
-                          skip = 3,
-                          colClasses= c("character", "character")) %>%
-  mutate(dt = mdy_hms(V1))
+hobo2data2024 <- read.csv('Hobo_2_2024field.csv', sep = ",", header = FALSE, skip = 3,
+                          colClasses= c("character", "character", "character", "character", "character"),
+                          na.strings = c("Logged", "Series: T-Type")) %>%
+  mutate(datetime = mdy_hms(V1)) %>%
+  mutate(across(starts_with("V"), as.numeric))  # Convert temp columns to numeric
 
-# Read data from Hobo3 CSV file
-hobo3data2024 <- read.csv('Hobo_3_2024field.csv',
-                          sep = ',',
-                          header = FALSE,
-                          skip = 3,
-                          colClasses= c("character", "character")) %>%
-  mutate(dt = mdy_hms(V1))
-# Combine first 2 data frames from hobo loggers
-combined_hobo1_hobo2 <- merge(hobo1data2024, hobo2data2024, by = 'dt' , all = TRUE)
+hobo3data2024 <- read.csv('Hobo_3_2024field.csv', sep = ",", header = FALSE, skip = 3,
+                          colClasses= c("character", "character", "character", "character", "character"),
+                          na.strings = c("Logged", "Series: T-Type")) %>%
+  mutate(datetime = mdy_hms(V1)) %>%
+  mutate(across(starts_with("V"), as.numeric))  # Convert temp columns to numeric
 
-# Combine 1st merge with last data set
-combined_loggers <- merge(combined_hobo1_hobo2, hobo3data2024, by = 'dt' , all = TRUE)
+# Rename relevant columns that we want to graph
+colnames(hobo1data2024) <- c("datetime", "temp1", "temp2", "temp3", "temp4")
+colnames(hobo2data2024) <- c("datetime", "temp5", "temp6", "temp7", "temp8")
+colnames(hobo3data2024) <- c("datetime", "temp9", "temp10", "temp11", "temp12")
 
-# Omit rows with any NA values
-cleaned_loggers <- drop_na(combined_loggers, dt)
+# Merge all datasets by datetime
+combined_loggers <- full_join(hobo1data2024, hobo2data2024, by = "datetime") %>%
+  full_join(., hobo3data2024, by = "datetime")
 
-# Remove unnecessary columns from dataset
-cleaned_loggers <- combined_loggers[ , !(names(combined_loggers) %in% c("V6.x", "V7.x", "V8.x", "V9.x", "V10.x", "V11.x", "V12.x", "V6.y", "V7.y", "V8.y", "V9.y", "V10.y", "V11.y", "V12.y", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13"))]
+# Drop rows with NA in datetime or temperature columns
+cleaned_loggers <- combined_loggers %>% drop_na(datetime)
+
+# Reshape the data to long format for plotting
+long_data <- cleaned_loggers %>%
+  pivot_longer(cols = starts_with("temp"), names_to = "Logger", values_to = "Temperature")
+
+# Plot the temperature data over time
+ggplot(long_data, aes(x = datetime, y = Temperature, color = Logger)) +
+  geom_line() +
+  labs(title = "Temperature Data from Multiple Loggers",
+       x = "Date and Time", y = "Temperature (°C)", color = "Logger") +
+  theme_minimal()
+
+# Create a heatmap for a different visualization
+ggplot(long_data, aes(x = datetime, y = Logger, fill = Temperature)) +
+  geom_tile() +
+  labs(title = "Heatmap of Logger Measurements Over Time",
+       x = "Date and Time", y = "Logger") +
+  theme_minimal() +
+  scale_fill_viridis_c()
 
