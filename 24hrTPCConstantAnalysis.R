d6754f7f3a2b7fc6723b47f22c213fdd2984c08b
Taylor Hatcher <129802014+taylorhatcher@users.noreply.github.com>
2024-11-04 12:58:24 -0800

    Create 24hrTPCConstantAnalysis.R

---
+++ 24hrTPCConstantAnalysis.R
@@ -0,0 +1,25 @@
+## Taylor M. Hatcher
+## Analysis of 6 hr and 24hr constant TPCs for P. rapae. Each caterpillar only experienced one temperature for 24 hours. 
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(patchwork)
+library(reshape2)
+library(viridis)
+library(tidyverse)
+
+# read in TPC data 
+tpccurrent = read.csv("2024PrapaeConstantTPCCombineddata.csv")
+# Convert fw to numeric and drop nas
+tpccurrent$fw <- as.numeric(tpccurrent$fw)
+
+tpccurrentrgr= (log(tpccurrent$fw) - log(tpccurrent$M0))/tpccurrent$duration
+
+
+
+
+
+
+tpccurrent <- tpccurrent %>% drop_na(fw)
+tpccurrent$rgr= (log(tpccurrent$fw) - log(tpccurrent$M0))/tpccurrent$duration
+
