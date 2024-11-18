cc15d51d091c347dd623bd298853619a7ada0691
Taylor Hatcher <129802014+taylorhatcher@users.noreply.github.com>
2024-11-18 12:10:07 -0800

    Update 24hrTPCConstantAnalysis.R

--- 24hrTPCConstantAnalysis.R
+++ 24hrTPCConstantAnalysis.R
@@ -9,6 +9,7 @@ library(viridis)
 library(tidyverse)
 
 setwd("~/Desktop/Repos/WARP2024")
+
 #past data 
 tpc1 = read.csv("PrapaeW.1999.ConstantTempTPCs.4thinstar.jul2021.xlsx - data.csv")
 tpc1$instar=4
@@ -26,8 +27,8 @@ tpc.ps= tpc.p[,c("UniID","mom","ID","temp","instar","time","duration","mgain","r
 #plot
 ggplot(tpc.ps[tpc.ps$time>5 & tpc.ps$time<11,], aes(x=time, color=factor(instar), group=factor(instar)))+
   geom_density()
-  table(tpc.ps$time, tpc.ps$instar)
-  
+table(tpc.ps$time, tpc.ps$instar)
+
 #recent 2024 TPC data
 tpc.c = read.csv("2024PrapaeConstantTPCCombineddata.csv")
 
@@ -64,28 +65,52 @@ tpc[tpc$time.per=="current","dur"]= tpc[tpc$time.per=="current","duration"]
 tpc.plot = ggplot(tpc[tpc$dur %in% c(6,24),], aes(x=temp, y=rgr, color=time.per)) +
   geom_point(alpha=0.5) +
   facet_grid(dur ~ instar) +
-  geom_smooth() +
   theme_bw() +
   xlab("Temperature (C)") +
   ylab("RGR (mg/mg/h)") +
   ggtitle("2024") +
   ylim(-0.02, 0.14)
-
 # Aggregate mean values with corrected standard error calculation
-tpc.agg <- tpc %>%
-  group_by(temp, time.per, dur, instar) %>%
-  dplyr::summarise(
-    mean = mean(rgr, na.rm = TRUE),
-    se = sd(rgr, na.rm = TRUE) / sqrt(n())
-  )
-tpc$time.per <- factor(tpc$time.per, levels = c("past", "current"))
-
-#plot family mean vaules 
-tpc.agg.f <- tpc %>%
-  group_by(FEMALE, temp) %>%
-  dplyr::summarise(mean = mean(value, na.rm = TRUE),
-                   se = sd(value, na.rm = TRUE)/length(value))
-
+    tpc.agg <- tpc %>%
+      group_by(temp, time.per, dur, instar) %>%
+      dplyr::summarise(
+        mean = mean(rgr, na.rm = TRUE),
+        se = sd(rgr, na.rm = TRUE) / sqrt(n())
+      )
+    tpc$time.per <- factor(tpc$time.per, levels = c("past", "current"))
+    
+    #plot with error bars
+    tpc.plot <- ggplot(tpc.agg[tpc.agg$dur %in% c(6, 24), ],aes(x = temp, y = mean, color = time.per, group = time.per)) +
+      geom_point(alpha = 0.7, size = 2) + # Points for mean values
+      geom_errorbar(aes(ymin = mean - se, ymax = mean +se), width = 0.3, alpha = 0.6) + # Error bars
+      facet_grid(dur ~ instar) + #Facets for duration and instar
+      theme_bw() +
+      xlab("Temperature(°C)") +
+      ylab("RGR (mg/mg/hr)") +
+      ggtitle("Past vs. Present Constant TPC")
+    ylim(-0.02, 0.14)
+    
+    
+    
+    # Fit a linear model and calculate predictions for each group
+    tpc.fit <- tpc.valid %>%
+      group_by(time.per, dur, instar) %>%
+      do({
+        model <- lm(mean ~ temp, data = .)
+        data.frame(
+          temp = seq(min(.$temp, na.rm = TRUE), max(.$temp, na.rm = TRUE), length.out = 100),  # Generate temperature sequence
+          mean = predict(model, newdata = data.frame(temp = seq(min(.$temp, na.rm = TRUE), max(.$temp, na.rm = TRUE), length.out = 100))),
+          time.per = unique(.$time.per),
+          dur = unique(.$dur),
+          instar = unique(.$instar)
+        )
+      })
+    
+    
+    
+    # Print the plot
+    print(tpc.plot)
+    
 setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
 pdf("2024TRNfeeding_constant.pdf",height = 8, width = 8)
 # Display the final plot
