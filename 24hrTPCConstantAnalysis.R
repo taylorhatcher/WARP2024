11d638d22dd1854d5057a22188f287fb89e49484
Taylor Hatcher <129802014+taylorhatcher@users.noreply.github.com>
2025-01-27 09:12:15 -0800

    Update 24hrTPCConstantAnalysis.R
    created new dataframa

--- 24hrTPCConstantAnalysis.R
+++ 24hrTPCConstantAnalysis.R
@@ -88,9 +88,6 @@ tpc.cs= tpc.c[,c("UniID","mom","ID","temp","active","instar","time","duration","
 # combine data sets 
 tpc= rbind.data.frame(tpc.cs, tpc.ps, sort = TRUE)
 
-
-###Plot
-
 ##Set up durations of 6hr and 24hr
 tpc.p$duration <- tpc.p$time
 tpc$dur=NA
@@ -104,113 +101,119 @@ tpc <- tpc%>%mutate(durbin =
                         duration > 5 & duration < 7 ~ 6,
                         duration > 23 & duration < 25 ~ 24,
                       ))
-###separate historic and present data
 
-# Filter data for historic data set 
-tpc_past <- tpc %>% filter(time.per == "past")
-tpc_past <- tpc %>% filter(active == "y")
+# Filter only active caterpillars to final data frame
+tpc <- tpc %>% filter(active == "y")
 
-# Create the plot for past data
-past_plot <- ggplot(tpc_past[tpc_past$dur %in% c(6, 24),], aes(x = temp, y = rgr, color = mom)) +
-  geom_point() +
-  #geom_line(aes(group = mom), alpha = 0.7) +
-  facet_grid(dur ~ instar) +
-  theme_bw() +
-  xlab("Temperature (C)") +
-  ylab("RGR (mg/mg/h") +
-  ggtitle("Historic Data (1999)") +
-  scale_color_viridis(discrete = TRUE, option = "C")
-print(past_plot)
-setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
-pdf("24hrTPCConstantHistoricData")
+# Save data frame to new Csv
 
-# Filter data for current data set
-tpc_current <- tpc %>% filter(time.per == "current")
-tpc_current <- tpc %>% filter(active == "y")
-
-
-# Filter out nas introduced by calculating growth rate and filter out values where the caterpillars were not active
-tpc_current_filtered <- tpc_current %>%
-  filter(!(is.na(rgr) & durbin == 0)) 
-
-# Exclude rows where rgr <= 0 or rgr is NA
-tpc_current_filtered <- tpc_current_filtered %>%
-  filter(rgr > 0 & !is.na(rgr))
-
-#check how many have NAs
-sum(is.na(tpc_current_filtered$mom))
-sum(is.na(tpc_current_filtered$temp))
-sum(is.na(tpc_current_filtered$rgr))
-sum(is.na(tpc_current_filtered$durbin))
-sum(is.na(tpc_current_filtered$instar))
-
-# Create the plot for current data
-current_plot <- ggplot(tpc_current_filtered, aes( x = temp, y = rgr, color = mom)) +
- geom_point() +
- # geom_line(aes(group = mom), alpha = 0.7) +
-  facet_grid(durbin ~ instar) +
-  theme_bw() +
-  xlab("Temperature (C)") +
-  ylab("RGR (mg/mg/h") +
-  ggtitle("Present Data (2024)") +
-  ylim(0, 0.17) +
-  scale_color_viridis(discrete = TRUE)
-print(current_plot)
-setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
-pdf("2024_24hrTPCConstantCurrentData")
-current_plot
-dev.off()
-
-
-#combine both individual plots for past and current data
-combined_plot <- past_plot + current_plot + plot_layout(ncol = 1)
-print(combined_plot)   
-tpc.plot <- ggplot(tpc[tpc$dur %in% c(6,24),], aes(x=temp, y=rgr, color=time.per)) +
-  geom_point(alpha=0.5) +
-  #geom_line(aes(group = mom, color = time.per), alpha = 0.7) + # lineage lines
-  facet_grid(dur ~ instar) +
-  theme_bw() +
-  xlab("Temperature (C)") +
-  ylab("RGR (mg/mg/h)") +
-  ggtitle("2024 vs. 1999 Constant TPC") +
-  ylim(-0.02, 11)+
-  scale_color_manual(values = c("current" = "#EE6A50", "past" = "#7AC5CD"))
-print(tpc.plot)
-setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
-pdf("2024ConstantTPCComparison")
-combined_plot
-dev.off()
-
-# Aggregate mean values with standard error calculation
-tpc.agg <- tpc %>%
-  group_by(temp, time.per, dur, instar) %>% # need to actually calculate the precise durations and not just a flat 6 hr and 24 hr durations for the present data set
-  dplyr::summarise(
-    mean = mean(rgr, na.rm = TRUE),
-    se = sd(rgr, na.rm = TRUE) / sqrt(n())
-  )
-#plotting family means with error bars
-tpc.plot <- ggplot(tpc.agg[tpc.agg$dur %in% c(6, 24), ],aes(x = temp, y = mean, color = time.per, group = time.per)) +
-  geom_point(alpha = 0.7, size = 2) + # Points for mean values
-  geom_errorbar(aes(ymin = mean - se, ymax = mean +se), width = 0.3, alpha = 0.6) + # Error bars
-  facet_grid(dur ~ instar) + #Facets for duration and instar
-  theme_bw() +
-  xlab("Temperature(°C)") +
-  ylab("RGR (mg/mg/hr)") +
-  ggtitle("Past vs. Present Constant TPC") +
-  ylim(-0.10, 0.14) +
-  scale_color_manual(values = c("current" = "#EE6A50", "past" = "#7AC5CD"))
-  #tpc$time.per <- factor(tpc$time.per, levels = c("past", "current")) 
-  print(tpc.plot)
-  setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
-  pdf("2024ConstantTPCFamilyMeans")
-  tpc.plot
-  dev.off()
-
-#plot family mean values 
-tpc.agg.f <- tpc %>%
-  group_by(mom) %>%
-  dplyr::summarise(rgr, na.rm=TRUE)
-
-sum(is.na(tpc_current$rgr))
-sum(is.na(tpc_past$rgr))
-
+# ###separate historic and present data
+# 
+# # Filter data for historic data set 
+# tpc_past <- tpc %>% filter(time.per == "past")
+# tpc_past <- tpc %>% filter(active == "y")
+# 
+# # Create the plot for past data
+# past_plot <- ggplot(tpc_past[tpc_past$dur %in% c(6, 24),], aes(x = temp, y = rgr, color = mom)) +
+#   geom_point() +
+#   #geom_line(aes(group = mom), alpha = 0.7) +
+#   facet_grid(dur ~ instar) +
+#   theme_bw() +
+#   xlab("Temperature (C)") +
+#   ylab("RGR (mg/mg/h") +
+#   ggtitle("Historic Data (1999)") +
+#   scale_color_viridis(discrete = TRUE, option = "C")
+# print(past_plot)
+# setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
+# pdf("24hrTPCConstantHistoricData")
+# 
+# # Filter data for current data set
+# tpc_current <- tpc %>% filter(time.per == "current")
+# tpc_current <- tpc %>% filter(active == "y")
+# 
+# 
+# # Filter out nas introduced by calculating growth rate and filter out values where the caterpillars were not active
+# tpc_current_filtered <- tpc_current %>%
+#   filter(!(is.na(rgr) & durbin == 0)) 
+# 
+# # Exclude rows where rgr <= 0 or rgr is NA
+# tpc_current_filtered <- tpc_current_filtered %>%
+#   filter(rgr > 0 & !is.na(rgr))
+# 
+# #check how many have NAs
+# sum(is.na(tpc_current_filtered$mom))
+# sum(is.na(tpc_current_filtered$temp))
+# sum(is.na(tpc_current_filtered$rgr))
+# sum(is.na(tpc_current_filtered$durbin))
+# sum(is.na(tpc_current_filtered$instar))
+# 
+# # Create the plot for current data
+# current_plot <- ggplot(tpc_current_filtered, aes( x = temp, y = rgr, color = mom)) +
+#  geom_point() +
+#  # geom_line(aes(group = mom), alpha = 0.7) +
+#   facet_grid(durbin ~ instar) +
+#   theme_bw() +
+#   xlab("Temperature (C)") +
+#   ylab("RGR (mg/mg/h") +
+#   ggtitle("Present Data (2024)") +
+#   ylim(0, 0.17) +
+#   scale_color_viridis(discrete = TRUE)
+# print(current_plot)
+# setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
+# pdf("2024_24hrTPCConstantCurrentData")
+# current_plot
+# dev.off()
+# 
+# 
+# #combine both individual plots for past and current data
+# combined_plot <- past_plot + current_plot + plot_layout(ncol = 1)
+# print(combined_plot)   
+# tpc.plot <- ggplot(tpc[tpc$dur %in% c(6,24),], aes(x=temp, y=rgr, color=time.per)) +
+#   geom_point(alpha=0.5) +
+#   #geom_line(aes(group = mom, color = time.per), alpha = 0.7) + # lineage lines
+#   facet_grid(dur ~ instar) +
+#   theme_bw() +
+#   xlab("Temperature (C)") +
+#   ylab("RGR (mg/mg/h)") +
+#   ggtitle("2024 vs. 1999 Constant TPC") +
+#   ylim(-0.02, 11)+
+#   scale_color_manual(values = c("current" = "#EE6A50", "past" = "#7AC5CD"))
+# print(tpc.plot)
+# setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
+# pdf("2024ConstantTPCComparison")
+# combined_plot
+# dev.off()
+# 
+# # Aggregate mean values with standard error calculation
+# tpc.agg <- tpc %>%
+#   group_by(temp, time.per, dur, instar) %>% # need to actually calculate the precise durations and not just a flat 6 hr and 24 hr durations for the present data set
+#   dplyr::summarise(
+#     mean = mean(rgr, na.rm = TRUE),
+#     se = sd(rgr, na.rm = TRUE) / sqrt(n())
+#   )
+# #plotting family means with error bars
+# tpc.plot <- ggplot(tpc.agg[tpc.agg$dur %in% c(6, 24), ],aes(x = temp, y = mean, color = time.per, group = time.per)) +
+#   geom_point(alpha = 0.7, size = 2) + # Points for mean values
+#   geom_errorbar(aes(ymin = mean - se, ymax = mean +se), width = 0.3, alpha = 0.6) + # Error bars
+#   facet_grid(dur ~ instar) + #Facets for duration and instar
+#   theme_bw() +
+#   xlab("Temperature(°C)") +
+#   ylab("RGR (mg/mg/hr)") +
+#   ggtitle("Past vs. Present Constant TPC") +
+#   ylim(-0.10, 0.14) +
+#   scale_color_manual(values = c("current" = "#EE6A50", "past" = "#7AC5CD"))
+#   #tpc$time.per <- factor(tpc$time.per, levels = c("past", "current")) 
+#   print(tpc.plot)
+#   setwd('/Volumes/GoogleDrive/Shared drives/TrEnCh/Projects/WARP/Analyses/figures/')
+#   pdf("2024ConstantTPCFamilyMeans")
+#   tpc.plot
+#   dev.off()
+# 
+# #plot family mean values 
+# tpc.agg.f <- tpc %>%
+#   group_by(mom) %>%
+#   dplyr::summarise(rgr, na.rm=TRUE)
+# 
+# sum(is.na(tpc_current$rgr))
+# sum(is.na(tpc_past$rgr))
+# 
